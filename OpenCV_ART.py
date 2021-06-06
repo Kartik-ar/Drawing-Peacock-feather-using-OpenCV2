4c0f57b144dcafb778ebdca2bd2721a500748d8f
Kartik-ar <71692764+Kartik-ar@users.noreply.github.com>
2021-06-06 20:39:54 +0530

    Update OpenCV_ART.py

--- OpenCV_ART.py
+++ OpenCV_ART.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-# In[2]:
-
 
 # Creating Plain Black Image
 art = np.zeros((1300,1300,3))
-
-
-# In[3]:
-
 
 # Variable used in creating image inside plain black Image
 a = 50
@@ -26,10 +14,6 @@ x,y,z = 590,570,820
 q,w,e,r = 320,790,550,820
 n,m,o = 160,110,170
 c,d,v,f = 120,150,140,170
-
-
-# In[4]:
-
 
 # drawing Peacock Feather using some manipulations
 for i in range(1,100,5):
@@ -41,11 +25,6 @@ for i in range(1,200,5):
     art[800:800+i, 500+i:800] = [0,255,0]
 for i in range(1,200,5):
     art[800:800+i, 550+i:950] = [0,0,0]
-
-
-# In[5]:
-
-
 for i in range(50):
     cv2.circle(art,(220,290),i,(0,255,0),3)
     cv2.circle(art,(210,240),i,(0,255,0),3)
@@ -112,24 +91,7 @@ for i in range(50):
     cv2.circle(art,(380,630),i,(255,0,0),3)
     cv2.circle(art,(430,600),i,(255,0,0),3)
     
-
-
-# In[6]:
-
-
-plt.imshow(art)
-
-
-# In[7]:
-
-
+# Displaying Drawn Image
 cv2.imshow('My Drawing', art)
 cv2.waitKey(10)
 cv2.destroyAllWindows()
-
-
-# In[ ]:
-
-
-
-
